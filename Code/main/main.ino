b05564048ca3a7c515251a65f9c6988423675447
Péter Molnár <xodax9898@gmail.com>
2023-06-05 00:16:18 +0200

    feat(Arduino): Logic of the line following car IR Sensors are working rn DC Motors are dependent to the IR sensors

---
+++ Code/main/main.ino
@@ -0,0 +1,137 @@
+
+
+// Motor A connections
+int enA = 9;
+int in1 = 8;
+int in2 = 7;
+// Motor B connections
+int enB = 3;
+int in3 = 5;
+int in4 = 4;
+
+void setup() {
+	// Set all the motor control pins to outputs
+	pinMode(enA, OUTPUT);
+	pinMode(enB, OUTPUT);
+	pinMode(in1, OUTPUT);
+	pinMode(in2, OUTPUT);
+	pinMode(in3, OUTPUT);
+	pinMode(in4, OUTPUT);
+
+	pinMode(A0, INPUT);
+	pinMode(A1, INPUT);
+
+  // pinMode(13, OUTPUT);
+  // pinMode(12, OUTPUT);
+
+  //Set motor speed
+  analogWrite(enA, 230);
+	analogWrite(enB, 230);
+	
+	// Turn off motors - Initial state
+  digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, LOW);
+
+  delay(5000);
+
+	// digitalWrite(in1, LOW);
+	// digitalWrite(in2, LOW);
+	// digitalWrite(in3, LOW);
+	// digitalWrite(in4, LOW);
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Serial.println(analogRead(A0));
+  if(analogRead(A1) < 500){
+    digitalWrite(in1, HIGH);
+  } else {
+     digitalWrite(in1, LOW);
+  }
+
+  if(analogRead(A0) < 500){
+    digitalWrite(in3, HIGH);
+  } else {
+    digitalWrite(in3, LOW);
+  }
+
+	// directionControl();
+	// speedControl();
+	// delay(1000);
+
+  // analogWrite(enA, 255);
+	// analogWrite(enB, 255);
+
+  // digitalWrite(in1, HIGH);
+	// digitalWrite(in2, LOW);
+	// digitalWrite(in3, HIGH);
+	// digitalWrite(in4, LOW);
+
+  // delay(5000);
+  
+	// digitalWrite(in1, LOW);
+	// digitalWrite(in2, HIGH);
+	// digitalWrite(in3, LOW);
+	// digitalWrite(in4, HIGH);
+  // delay(2000);
+}
+
+// This function lets you control spinning direction of motors
+void directionControl() {
+	// Set motors to maximum speed
+	// For PWM maximum possible values are 0 to 255
+	analogWrite(enA, 255);
+	analogWrite(enB, 255);
+
+	// Turn on motor A & B
+	digitalWrite(in1, HIGH);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, HIGH);
+	digitalWrite(in4, LOW);
+	delay(2000);
+	
+	// Now change motor directions
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, HIGH);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, HIGH);
+	delay(2000);
+	
+	// Turn off motors
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, LOW);
+}
+
+// This function lets you control speed of the motors
+void speedControl() {
+	// Turn on motors
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, HIGH);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, HIGH);
+	
+	// Accelerate from zero to maximum speed
+	for (int i = 0; i < 256; i++) {
+		analogWrite(enA, i);
+		analogWrite(enB, i);
+		delay(20);
+	}
+	
+	// Decelerate from maximum speed to zero
+	for (int i = 255; i >= 0; --i) {
+		analogWrite(enA, i);
+		analogWrite(enB, i);
+		delay(20);
+	}
+	
+	// Now turn off motors
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, LOW);
+}
