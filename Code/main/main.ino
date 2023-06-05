b6b1dc31175b149ca19815aa083b464446059279
ivan-akos <114164620+ivan-akos@users.noreply.github.com>
2023-06-05 21:38:46 +0200

    led controls

--- Code/main/main.ino
+++ Code/main/main.ino
@@ -32,8 +32,20 @@ void setup() {
 	pinMode(A0, INPUT);
 	pinMode(A1, INPUT);
 
+  pinMode(ledARed, OUTPUT);
+  pinMode(ledAGreen, OUTPUT);
+  pinMode(ledABlue, OUTPUT);
+
+  pinMode(ledBRed, OUTPUT);
+  pinMode(ledBGreen, OUTPUT);
+  pinMode(ledBBlue, OUTPUT);
+
+  driveR = false;
+  driveL = false;
 
   //Set motor speed
+  analogWrite(enA, 80);
+	analogWrite(enB, 80);
 	
 	// Turn off motors - Initial state
   digitalWrite(in1, LOW);
@@ -41,7 +53,10 @@ void setup() {
 	digitalWrite(in3, LOW);
 	digitalWrite(in4, LOW);
 
+  setALOW();
+  setBLOW();
 
+  delay(5000);
   
   Serial.begin(9600);
 }
@@ -49,18 +64,63 @@ void setup() {
 void loop() {
   if(analogRead(A1) < 500){
     digitalWrite(in1, HIGH);
+    driveL = true;
   } else {
      digitalWrite(in1, LOW);
+     driveL = false;
   }
 
   if(analogRead(A0) < 500){
     digitalWrite(in3, HIGH);
+    driveR = true;
   } else {
     digitalWrite(in3, LOW);
+    driveR = false;
   }
 
+  if(driveL == true && driveR == true){
+    setALOW();
+    setBLOW();
+  } else if (driveL == true && driveR == false){
+    setARed();
+    setBLOW();
+  } else if (driveL == false && driveR == true){
+    setBRed();
+    setALOW();
+  } else if (driveL == false && driveR == false ){
+    setABlue();
+    setBBlue();
+  }
 }
 
+void setALOW(){
+  digitalWrite(ledABlue, LOW);
+  digitalWrite(ledAGreen, LOW);
+  digitalWrite(ledARed, LOW);
+}
+void setARed(){
+  digitalWrite(ledABlue, LOW);
+  digitalWrite(ledAGreen, LOW);
+  digitalWrite(ledARed, HIGH);
+}
+void setABlue(){
+  digitalWrite(ledAGreen, LOW);
+  digitalWrite(ledARed, LOW);
+  digitalWrite(ledABlue, HIGH);
 }
 
+void setBLOW(){
+  digitalWrite(ledBBlue, LOW);
+  digitalWrite(ledBGreen, LOW);
+  digitalWrite(ledBRed, LOW);
+}
+void setBRed(){
+  digitalWrite(ledBBlue, LOW);
+  digitalWrite(ledBGreen, LOW);
+  digitalWrite(ledBRed, HIGH);
+}
+void setBBlue(){
+  digitalWrite(ledBGreen, LOW);
+  digitalWrite(ledBRed, LOW);
+  digitalWrite(ledBBlue, HIGH);
 }
