b890e736529450c197f0a13ed675ce51dee69bfe
ivan-akos <114164620+ivan-akos@users.noreply.github.com>
2023-06-05 21:37:45 +0200

    removed unused stuff

--- Code/main/main.ino
+++ Code/main/main.ino
@@ -21,12 +21,8 @@ void setup() {
 	pinMode(A0, INPUT);
 	pinMode(A1, INPUT);
 
-  // pinMode(13, OUTPUT);
-  // pinMode(12, OUTPUT);
 
   //Set motor speed
-  analogWrite(enA, 230);
-	analogWrite(enB, 230);
 	
 	// Turn off motors - Initial state
   digitalWrite(in1, LOW);
@@ -34,18 +30,12 @@ void setup() {
 	digitalWrite(in3, LOW);
 	digitalWrite(in4, LOW);
 
-  delay(5000);
 
-	// digitalWrite(in1, LOW);
-	// digitalWrite(in2, LOW);
-	// digitalWrite(in3, LOW);
-	// digitalWrite(in4, LOW);
   
   Serial.begin(9600);
 }
 
 void loop() {
-  // Serial.println(analogRead(A0));
   if(analogRead(A1) < 500){
     digitalWrite(in1, HIGH);
   } else {
@@ -58,80 +48,8 @@ void loop() {
     digitalWrite(in3, LOW);
   }
 
-	// directionControl();
-	// speedControl();
-	// delay(1000);
-
-  // analogWrite(enA, 255);
-	// analogWrite(enB, 255);
-
-  // digitalWrite(in1, HIGH);
-	// digitalWrite(in2, LOW);
-	// digitalWrite(in3, HIGH);
-	// digitalWrite(in4, LOW);
-
-  // delay(5000);
-  
-	// digitalWrite(in1, LOW);
-	// digitalWrite(in2, HIGH);
-	// digitalWrite(in3, LOW);
-	// digitalWrite(in4, HIGH);
-  // delay(2000);
 }
 
-// This function lets you control spinning direction of motors
-void directionControl() {
-	// Set motors to maximum speed
-	// For PWM maximum possible values are 0 to 255
-	analogWrite(enA, 255);
-	analogWrite(enB, 255);
-
-	// Turn on motor A & B
-	digitalWrite(in1, HIGH);
-	digitalWrite(in2, LOW);
-	digitalWrite(in3, HIGH);
-	digitalWrite(in4, LOW);
-	delay(2000);
-	
-	// Now change motor directions
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, HIGH);
-	digitalWrite(in3, LOW);
-	digitalWrite(in4, HIGH);
-	delay(2000);
-	
-	// Turn off motors
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, LOW);
-	digitalWrite(in3, LOW);
-	digitalWrite(in4, LOW);
 }
 
-// This function lets you control speed of the motors
-void speedControl() {
-	// Turn on motors
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, HIGH);
-	digitalWrite(in3, LOW);
-	digitalWrite(in4, HIGH);
-	
-	// Accelerate from zero to maximum speed
-	for (int i = 0; i < 256; i++) {
-		analogWrite(enA, i);
-		analogWrite(enB, i);
-		delay(20);
-	}
-	
-	// Decelerate from maximum speed to zero
-	for (int i = 255; i >= 0; --i) {
-		analogWrite(enA, i);
-		analogWrite(enB, i);
-		delay(20);
-	}
-	
-	// Now turn off motors
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, LOW);
-	digitalWrite(in3, LOW);
-	digitalWrite(in4, LOW);
 }
