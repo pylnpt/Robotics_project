b4f517106fcefdbb711c2fc37e91370c19ff6660
ivan-akos <114164620+ivan-akos@users.noreply.github.com>
2023-06-05 21:37:55 +0200

    led connections

--- Code/main/main.ino
+++ Code/main/main.ino
@@ -8,6 +8,17 @@ int in2 = 7;
 int enB = 3;
 int in3 = 5;
 int in4 = 4;
+//Led A connections
+int ledARed = 10;
+int ledAGreen = 11;
+int ledABlue = 12; 
+
+int ledBRed = 2;
+int ledBGreen = 6;
+int ledBBlue = 13; 
+
+bool driveR;
+bool driveL;
 
 void setup() {
 	// Set all the motor control pins to outputs
